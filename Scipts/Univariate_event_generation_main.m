a607ad4a368719d50dd94cac5b902666d3ccf222
Pravin Maduwantha <158492664+pr109704@users.noreply.github.com>
2024-09-13 17:00:17 -0400

    Update Univariate_event_generation_main.m

--- Scipts/Univariate_event_generation_main.m
+++ Scipts/Univariate_event_generation_main.m
@@ -2,7 +2,7 @@
 %   Author: Pravin
 %
 %   This Script is the main program for calculating return periods and
-%   scaling historical events to gievn return periods
+%   scaling historical events to given return periods
 %   
 %   IMPORTANT: The paths included in the script are according to the
 %   author's directory. Please change them accordingly
@@ -65,7 +65,7 @@ Hour_no=28; % for the purpose of plotting
 Acc=18; % The selected rainfall accumulation
 Cycl_name ={'Sandy','Irene','Floyd'}; % Three TC events are considered
 % Selected event = Irene (rank 2)
-% For sandy; The NTR rank is 2 ( The second largest events based on peak NTR)
+% For sandy; The NTR rank is 2 ( The second largest event based on peak NTR)
 
 Rank=2;
 Ex_events_TC_row = load("***Path***\TC_events_con_NTR_from_1991.mat");
@@ -77,11 +77,11 @@ Peak_hour =Ex_events_TC(Rank,1);
 Max_RF = Ex_events_TC(Rank,4);
 %Max_RF_hour = Ex_events_TC(Rank,3);
 
-% loadting NTR data
+% loading NTR data
 NTR=load("***Path***\POT_NTR_and_NTR_timeseries_for_Philli_airport.mat");
 % Loading the RF fields time series over the Gloucester City
 RF_Field = load("***Path***\AORC_Gloucester_city_cliped_data.mat");
-% importing basin avearge AORC Data 
+% importing basin average AORC Data 
 AORC = load("***Path***\Hourly_accumulation_RF_data_Gloucester_City_AORC.mat");
 AORC=AORC.Data;
 
@@ -106,17 +106,8 @@ BA_AORC_hourly = [AORC(:,1) AORC(:,2)];
 ind_peak_RF = find(BA_AORC_hourly(:,1)==Peak_hour);
 BA_AORC_event_hourly = BA_AORC_hourly(ind_peak_RF-36:ind_peak_RF+35,1:2);
 
-figure
-plot(datetime(datevec(Day_3_ntr(:,1))),Day_3_ntr(:,2));hold on;
-plot(datetime(datevec(Day_3_ntr(:,1))),D3_tide);
-plot(datetime(datevec(Day_3_ntr(:,1))),D3_WL);
-ylim([-1 8])
 
-yyaxis right
-plot(datetime(datevec(BA_AORC_event_hourly(:,1))),BA_AORC_event_hourly(:,2));
-
-
-%% Scalling Rainfall fields and NTR time series of the event
+%% Scaling Rainfall fields and NTR time series of the event
 
 
 for i=1:length(RL_NTR)    
@@ -130,71 +121,15 @@ for i=1:length(RL_NTR)
     min_NTR = min(Day_3_ntr(:,2));
     Day_3_ntr_datum_adj = Day_3_ntr(:,2)+abs(min_NTR);
     
-    % calculating the scalling factor
+    % calculating the scaling factor
     Scale_NTR(i,1) = (RL_NTR(2,i)+abs(min_NTR))/(Max_NTR+abs(min_NTR));
     Design_NTR_scaled(i).event = [Day_3_ntr(:,1) (Day_3_ntr_datum_adj.*Scale_NTR(i,1)-abs(min_NTR))];
   
 end
 
-%%%%%%%%%%%%%%%%%%%%%%%%%% Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% Plotting before creating the structure files
-% for NTR
 
 Data_raw = [Day_3_ntr(:,1) Day_3_ntr(:,2)];
 T=datetime(datevec(Data_raw(:,1)));
-
-
-hh=figure;
-for i=1:length(RL_NTR)
-    Data=Design_NTR_scaled(i).event;
-    subplot(length(RL_NTR)/2,2,i);
-    plot(T, Data(:,2),'.-','LineStyle', "-","Color","r");hold on;
-    hold on
-    plot(T, Data_raw(:,2),'.-','LineStyle', "-","Color","b");hold on;
-    str = ['Return Period. ',num2str(RL_NTR(1,i))];   
-    text(min(xlim)+0.1,max(ylim)-0.1,str,"FontSize",9,"FontName",'Times New Roman', 'VerticalAlignment','top', 'HorizontalAlignment','left');
-    legend('Scaled','Actual');
-    ylabel('NTR (m)');set(gca,'FontSize',8,'FontName','Times New Roman');grid on
-    ax = gca;
-    ax.XTickLabelRotation= 0;
-    title('')
-    hold off
-
-end
-hh.Position=[0 0 1600 2000];
-legend('Scaled','Actual');
-xlabel(" Time (h) ");set(gca,'FontSize',8,'FontName','Times New Roman');
-sgtitle(['NTR time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman')
-saveas(hh,['NTR_of_Scaled_events_',Cycl_name{Rank},'.png'])
-
-
-% Ploting basin averaged Rainfall for events 
-hh=figure;
-
-for i=1:length(RL_NTR)
-    Data=BA_RF_scaled(i).event;
-    T=datetime(datevec(BA_AORC_event_hourly(:,1)));
-    subplot(length(RL_NTR)/2,2,i);    
-    plot(T, Data(:,2),'.-','LineStyle', "-","Color","r");hold on;
-    plot(T, BA_AORC_event_hourly(:,2),'.-','LineStyle', "-","Color","b"); 
-    set(gcf,'position',[0,0,1500,400],'Units','centimeters')
-    str = ['Return Period. ',num2str(RL_NTR(1,i))];   
-    text(min(xlim)+0.1,max(ylim)-0.1,str,"FontSize",9,"FontName",'Times New Roman', 'VerticalAlignment','top', 'HorizontalAlignment','left');
-    legend('Scaled','Actual');
-    ylabel('Hourly Rainfall (mm)'); xlabel(" Time ");set(gca,'FontSize',8,'FontName','Times New Roman');grid on
-    ax = gca;
-    ax.XTickLabelRotation= 0;
-    title('')
-    hold off
-    ylim([0 25]);
-end
-hh.Position=[0 0 1600 2000];
-legend('Scaled','Actual');
-xlabel(" Time (h) ");set(gca,'FontSize',8,'FontName','Times New Roman');
-sgtitle(['RF time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman')
-saveas(hh,['Basin average RF of scaled events_',Cycl_name{Rank},'.png'])
-
-%%%%%%%%%%%%%%%%%% end of Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% Fitting distribution to the WL moving average for the last 18.6 years 
@@ -241,78 +176,9 @@ for i=1:length(RL_NTR)
 end
 
 
-
-
-%% %%%%%%%%%%%%%%%%%% Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Plotting the generated events
-clearvars -except Peak_hour rp Cycl_name MSL_senario NTR Design_MSL Rank BA_AORC_event_hourly RL_NTR RL_RF Day_3_ntr
-
-WL_raw_ind= find(NTR.POT_NTR_and_NTR_timeseries.Time==Peak_hour);
-WL_raw = NTR.POT_NTR_and_NTR_timeseries.WL_raw(WL_raw_ind-36:WL_raw_ind+35);
-MA_wl = NTR.POT_NTR_and_NTR_timeseries.MovA_WL(WL_raw_ind-36:WL_raw_ind+35);
-
-%  NTR
-T=1:1:72;
-T=T';
-kk=1;
-dim = [.2 .5 .3 .3];
-
-for kk=1:length(Design_MSL)
-    Data=load (['Design_events_',Cycl_name{Rank},'.mat']); 
-    hh=figure;
-    for i=1:length(RL_NTR)
-        data_wl= Data.Design_event(i).event.Water_level;
-        subplot(length(RL_NTR)/2,2,i);
-        plot(T, data_wl,'.-','LineStyle', "-","Color","r");hold on;
-        hold on
-        plot(T, WL_raw,'.-','LineStyle', "-","Color","b");hold on;               
-        str = ['Return Period. ',num2str(RL_NTR(1,i))];       
-        text(0.2,1.8,str,"FontSize",9,"FontName",'Times New Roman');
-        %set(gca,'XTickLabel',[])
-        ylabel('Water Level (m)');
-        set(gca,'FontSize',10,'FontName','Times New Roman')
-        grid on
-        ax = gca;
-        hold off
-    end
-    hh.Position=[0 0 1600 2000];
-    legend('Scaled','Actual');
-    xlabel(" Time (h) ");
-    sgtitle(['Water Level time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman')
-    saveas(hh,['WL_of_Design_events_',Cycl_name{Rank},'.png'])
     
-%     Ploting basin averaged Rainfall for events 
-    
-    hhh=figure;
-    for i=1:length(RL_NTR)
-        data_RF= Data.Design_event(i).event.Rainfall;
-        avg_data_RF(:,1)=mean(mean(data_RF(:,:,:),2),1);    
-        subplot(length(RL_NTR)/2,2,i);       
-        plot(T, avg_data_RF,'.-','LineStyle', "-","Color","r");hold on;
-        plot(T, BA_AORC_event_hourly(:,2),'.-','LineStyle', "-","Color","b");        
-        str = ['Return Period. ',num2str(RL_NTR(1,i))];
-        text(0.2,28,str,"FontSize",9,"FontName",'Times New Roman', 'VerticalAlignment','top', 'HorizontalAlignment','left');     
-        legend('Scaled','Actual');
-        ylabel('Hourly Rainfall (mm)');set(gca,'FontSize',10,'FontName','Times New Roman');grid on
-        ax = gca;
-        ax.XTickLabelRotation= 0;
-        title('')
-        hold off
-        ylim([0 30]);
-  
-    end
-    hhh.Position=[0 0 1600 2000];
-    legend('Scaled','Actual');
-    xlabel(" Time (h) ");set(gca,'FontSize',10,'FontName','Times New Roman');xticks(0:12:72); xticklabels(0:12:72);
-    sgtitle(['Basin Average Rainfall time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman');
-    saveas(hhh,['RF_of_Design_events_',Cycl_name{Rank},'.png'])
-    clearvars Design_event data_wl hhh hh
-end
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% end of plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    
-%%  Defining the starting and eneding time manually 
-% these hours were checked and selected manualy to optimize the
+%%  Defining the starting and ending time manually 
+% These hours were checked and selected manually to optimize the
 % computational time of the numerical modeling
 
 st = 14; % Starting hour
@@ -330,71 +196,4 @@ end
 
 save((['Design_events_Cropped_',Cycl_name{Rank}]),'Design_events');
 
-
-
-
-%% %%%%%%%%%%%%%%%%%%%%  Plotting %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Plotting the Cropped events
-
-WL_raw_ind= find(NTR.POT_NTR_and_NTR_timeseries.Time==Peak_hour);
-WL_raw = NTR.POT_NTR_and_NTR_timeseries.WL_raw(WL_raw_ind-36:WL_raw_ind+35);
-MA_wl = NTR.POT_NTR_and_NTR_timeseries.MovA_WL(WL_raw_ind-36:WL_raw_ind+35);
-WL_raw=WL_raw(st:endd);
-%  NTR
-T=1:1:length(Design_events(1).WL);
-T=T';
-kk=1;
-dim = [.2 .5 .3 .3];
-
-for kk=1:length(Design_MSL)
-    Data=load (['Design_events_Cropped_',Cycl_name{Rank},'.mat']);
-    hh=figure;
-    for i=1:length(RL_NTR)
-        data_wl= Data.Design_events(i).WL;
-        subplot(length(RL_NTR)/2,2,i);
-        plot(T, data_wl,'.-','LineStyle', "-","Color","r");hold on;
-        hold on
-        plot(T, WL_raw,'.-','LineStyle', "-","Color","b");hold on;      
-        str = ['Return Period. ',num2str(RL_NTR(1,i))];   
-        text(0.2,1.8,str,"FontSize",9,"FontName",'Times New Roman');
-        %set(gca,'XTickLabel',[])
-        ylabel('Water Level (m)');
-        set(gca,'FontSize',10,'FontName','Times New Roman')
-        grid on
-        ax = gca;
-        hold off
-    end
-    hh.Position=[0 0 1600 2000];
-    legend('Scaled','Actual');
-    xlabel(" Time (h) ");
-    sgtitle(['Water Level time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman')
-    saveas(hh,['WL_of_Design_events_',Cycl_name{Rank},'.png'])
-    
-%     Ploting basin averaged Rainfall for events 
-    avg_data_RF=[];
-    hhh=figure;
-    for i=1:length(RL_NTR)
-        data_RF= Data.Design_events(i).Rainfall;
-        avg_data_RF(:,1)=mean(mean(data_RF(:,:,:),2),1);
-        subplot(length(RL_NTR)/2,2,i); 
-        plot(T, avg_data_RF,'.-','LineStyle', "-","Color","r");hold on;
-        plot(T, BA_AORC_event_hourly(st:endd,2),'.-','LineStyle', "-","Color","b"); 
-        str = ['Return Period. ',num2str(RL_NTR(1,i))];
-        text(0.2,28,str,"FontSize",9,"FontName",'Times New Roman', 'VerticalAlignment','top', 'HorizontalAlignment','left');       
-        legend('Scaled','Actual');
-        ylabel('Hourly Rainfall (mm)');set(gca,'FontSize',10,'FontName','Times New Roman');grid on
-        ax = gca;
-        ax.XTickLabelRotation= 0;
-        title('')
-        hold off
-        ylim([0 30]);
-  
-    end
-    hhh.Position=[0 0 1600 2000];
-    legend('Scaled','Actual');
-    xlabel(" Time (h) ");set(gca,'FontSize',10,'FontName','Times New Roman');xticks(0:12:72); xticklabels(0:12:72);
-    sgtitle(['Basin Average Rainfall time series scaled from : ',Cycl_name{Rank}],'FontSize',16,'FontName','Times New Roman');
-    saveas(hhh,['RF_of_Design_events_',Cycl_name{Rank},'.png'])    
-    clearvars Design_event data_wl hhh hh
-end
 
